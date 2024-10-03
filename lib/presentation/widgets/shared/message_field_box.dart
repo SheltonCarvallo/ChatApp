c2cddac850a2d4c779d00a491c0db7de62bd3c83
shelton <sheltoncarvallo975@gmail.com>
2024-10-02 22:45:06 -0500

    Added functions chat-box

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,30 +5,44 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
     final outlineInputBorder = UnderlineInputBorder(
         borderSide: const BorderSide(color: Colors.transparent),
         borderRadius: BorderRadius.circular(40));
 
     final inputDecoration = InputDecoration(
+      hintText: 'Fish up your message with a "?"',
       enabledBorder: outlineInputBorder,
       focusedBorder: outlineInputBorder,
       filled: true,
       suffixIcon: IconButton(
         icon: const Icon(Icons.send_outlined),
         onPressed: () {
-          print('Valor de la caja de texto');
+          final textValue = textController.value.text;
+          print('Valor de la caja de texto: $textValue');
+          textController.clear();
         },
       ),
     );
 
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
+        //Send the text
         print('Submit value $value');
+        textController.clear();
+        focusNode.requestFocus();
       },
-      onChanged: (value) {
+      /* onChanged: (value) {
+        //Get every character press un the keyboard
         print('changed: $value');
-      },
+      }, */
     );
   }
 }
